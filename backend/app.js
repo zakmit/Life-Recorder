7dbf9988a2e9a68280e9a1b4216279c6e8a0c695
zakmit <alcrolex7@gmail.com>
2019-08-20 15:37:28 +0800

    jade->pug

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,8 @@ var app = express();
 
 // view engine setup
 
+app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
